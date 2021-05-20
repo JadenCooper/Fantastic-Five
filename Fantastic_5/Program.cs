335058951bc3b55b1cc88b10e73b7a05d8e1282f
Kyle Black (1000026735) <BLACK11@student.op.ac.nz>
2021-05-20 15:16:22 +1200

    The first upload of the Visual Studio Project file

---
+++ Fantastic_5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fantastic_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
